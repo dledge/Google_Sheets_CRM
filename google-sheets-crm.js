102e4fc1fb84eb3bf417eef64c80b998755e011d
dledge <dledge@users.noreply.github.com>
2017-08-03 21:09:41 -0400

    Update google-sheets-crm.js
    Added additional functionality

--- google-sheets-crm.js
+++ google-sheets-crm.js
@@ -7,13 +7,15 @@ var oneDay = 24*60*60*1000;
 // Today
 var today = new Date();
 
+// My email address
+var myEmailAddress = Session.getActiveUser().getEmail();
+
 // Parameters
 
 // This script will write a block of 4 columns (date last contacted, days since contact, flagged?, did I reply?)
 // COLUMN_WHERE_MAGIC_BEGINS is the column where these four will be inserted.  Note that the first column has a value
 // of 1.
-
-var COLUMN_WHERE_MAGIC_BEGINS = 7;
+var COLUMN_WHERE_MAGIC_BEGINS = 10;
 
 // And this is the column where your email addresses are.  Again, a value of 1 means the first column.
 var COLUMN_WITH_EMAIL_ADDRESSES = 2;
@@ -30,6 +32,9 @@ function runSimpleCRM() {
     cache.put("lastRow", lastRowProcessed, 60*60*24); // cache for 25 minutes
   }  
   
+  // Keep track of where we started 
+  var startingRow = lastRowProcessed;
+  
   // Connect to our active sheet and collect all of our email addresses in the second column
   var sheet = SpreadsheetApp.getActiveSheet();
   var totalRows = sheet.getLastRow();
@@ -45,18 +50,26 @@ function runSimpleCRM() {
     // Increment the row we're processing
     var currentRow = lastRowProcessed+1;
     
+    // If we've been through all records, bail
+    if (currentRow == startingRow) {
+      break; 
+    }
+    
     // Get the email address from the current row
-    var email = emails[currentRow-2][0];
+    var theirEmail = emails[currentRow-2][0].trim();
     
     // If the email address field is empty, skip to the next row
-    if (!email) {
+    if (!theirEmail) {
       lastRowProcessed = currentRow;
       cache.put("lastRow", currentRow, 60*60*24); 
       continue;
     } 
     
     // Look for all threads from me to this person
-    var threads = GmailApp.search('from:me to:'+email);
+    var threads = GmailApp.search('from:me to:'+theirEmail);
+    
+    // Add a quick pause so we don't run into rate limiting issues with gmail
+    Utilities.sleep(20);
     
     // If there are no threads, I haven't emailed them before
     if (threads.length == 0) {
@@ -64,7 +77,7 @@ function runSimpleCRM() {
       // Update the spreadsheet row to show we've never emailed
       var range = sheet.getRange(currentRow, COLUMN_WHERE_MAGIC_BEGINS,1, 4 ).setValues([["NEVER", "", "", ""]] );
       
-      //  And carry one
+      //  And cary one
       lastRowProcessed = currentRow;
       cache.put("lastRow", currentRow, 60*60*24); // cache for 25 minutes    
       continue;
@@ -74,8 +87,8 @@ function runSimpleCRM() {
     var latestDate = new Date(1970, 1, 1);
     
     var starredMsg = "";
-    var iReplied = ""
-    
+    var threadStatus = ""
+
     // Iterate through each of the message threads returned from our search
     for (var thread in threads) {
       
@@ -94,24 +107,55 @@ function runSimpleCRM() {
           starredMsg = "";
         }           
         
+        
         // Open the thread to see who was the last to speak
         var messages = threads[thread].getMessages();
+        var totalMessages = messages.length;
         var lastMsg = messages[messages.length-1];
         var lastMsgFrom = lastMsg.getFrom();
         
-        // Use regex so we can make our search case insensitive
-        var re = new RegExp(email,"i");
         
+        // Use regex so we can make our search case insensitive
+        var reTheirEmail = new RegExp(theirEmail,"i");
+        var reMyEmail = new RegExp(myEmailAddress,"i");
+
         // If we can find their email address in the email address from the last message, they spoke last
         // (we may be back to overwrite this)
-        if (lastMsgFrom.search(re) >= 0) {
-          iReplied = "N";
+        /*
+        var rawEmail = lastMsg.getRawContent();
+        var reCal1 = new RegExp('text\/calendar',"i");
+        var reCal2 = new RegExp('calendar-notification',"i");
+
+        if (theirEmail == "dledger@gmail.com") {
+          var hereWeStop = true; 
+          Logger.clear();
+          Logger.log(rawEmail);
+        }
+
+        
+        if (rawEmail.search('text/calendar') >= 0 || rawEmail.search('calendar-notification') >= 0) {
+          // Ignore it unless this is the only correspondence so far
+          threadStatus = "We were on a calendar invite together"
+        }         
+        else */
+        if (lastMsgFrom.search(reTheirEmail) >= 0) {
+          if (totalMessages == 1) {
+            threadStatus = "They emailed me, I haven't replied";
+          } else {
+            threadStatus = "We had an email exchange, they replied last";
+          }
+        } else if (lastMsgFrom.search(reMyEmail) >= 0) {
+          if (totalMessages == 1) {
+            threadStatus = "I emailed them, they haven't replied";
+          } else {
+            threadStatus = "We had an email exchange, I replied last";            
+          }          
         } else {
-          iReplied = "";
-        }       
+          threadStatus = "We were on a thread together, neither of us replied last";            
+        }
       }
     }
-    
+  
     // Determine how many days have passed since our last correspondence 
     var daysSinceContact = Math.round(Math.abs((today.getTime() - latestDate.getTime())/(oneDay)));
     
@@ -120,7 +164,7 @@ function runSimpleCRM() {
     var latestDate = Utilities.formatDate(latestDate, SpreadsheetApp.getActive().getSpreadsheetTimeZone(),  "MMM d yyyy");
     
     // Write the row!
-    var range = sheet.getRange(currentRow, COLUMN_WHERE_MAGIC_BEGINS, 1, 4 ).setValues([[latestDate, daysSinceContact, starredMsg, iReplied]] );
+    var range = sheet.getRange(currentRow, COLUMN_WHERE_MAGIC_BEGINS, 1, 4 ).setValues([[latestDate, daysSinceContact, starredMsg, threadStatus]] );
     
     // update cache
     cache.put("lastRow", currentRow, 60*60*24); 
@@ -132,3 +176,4 @@ function runSimpleCRM() {
     Logger.log("processed "+currentRow);   
   }
 }
+
